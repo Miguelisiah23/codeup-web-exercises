ddf3559393e159c1664796415f0786fb40dcb8b1
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-04-22 09:20:22 -0500

    fizz buzz warmup

---
+++ js/warmup_fizz_buzz.js
@@ -0,0 +1,13 @@
+"use strict";
+
+for (let x = 1; x <= 100; x++) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (x % 5 === 0) {
+        console.log("Buzz");
+    } else if (x % 3 === 0) {
+        console.log("Fizz");
+        continue;
+    }
+    console.log(x);
+}
--- js/while.js
+++ js/while.js
@@ -1,8 +1,8 @@
 "use strict";
 
-// while (true) {
+// while (x < 65536) {
 //     for(let x = 2; x <= 512; x * x) {
 //         console.log(x);
-//     }
+//     } for (let y = x; )
 //
 // }
