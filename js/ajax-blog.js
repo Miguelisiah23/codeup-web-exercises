37597ac7e0173287ab60aadc86e74cf75ea6cc22
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-05-25 13:52:19 -0500

    ajax exercise

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -2,12 +2,7 @@ $(function (){
     "use strict"
     $.get("../data/blog.json").done(function (posts){
         posts.forEach(function (post){
-            let blog = "<div class='brdr'>"+
-                "<h2>" + post.title + "</h2>"
-                + "<p>"+ post.date + "</p>" +
-                "<p>" + post.content +"</p>" +
-                "<p>"+post.categories+"</p>" +
-                "</div>"
+            let blog = "<div class='brdr'>"+ "<h2>" + post.title + "</h2>" + "<p>"+ post.date + "</p>" + "<p>" + post.content +"</p>" + "<p>"+post.categories+"</p>" + "</div>"
             $("#posts").append(blog)
         });
     });
