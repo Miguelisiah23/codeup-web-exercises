9bef3c14882de31b87ff84583650e5fa361d381e
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-06-06 12:16:51 -0500

    promises

--- js/promises.js
+++ js/promises.js
@@ -1,10 +1,14 @@
 "use strict";
 let username = prompt('Github username ?');
-const URL = `https://api.github.com/users/${username}/events/public`
-fetch(URL, {headers: {Authorization:`token ${GITHUB_API_KEY}`}}).then((resp => resp.json())).then((data) => {
-    console.log(data);
-    let message = `${data[0].actor.login}, Your last commit was ${data[0].created_at.substring(0,10)}.`;
-     let commitMsg = `Your commit message was '${data[0].payload.commits[0].message}'`;
-    document.getElementById("message").append(message);
-    document.getElementById("commit").append(commitMsg);
-});
+const lastCommit = (username) => {
+    const URL = `https://api.github.com/users/${username}/events/public`;
+
+    fetch(URL, {headers: {Authorization:`token ${GITHUB_API_KEY}`}}).then((resp => resp.json())).then((data) => {
+        let message = `${data[0].actor.login}, Your last commit was ${data[0].created_at.substring(0,10)}.`;
+        let commitMsg = `Your commit message was '${data[0].payload.commits[0].message}'`;
+        document.getElementById("message").append(message);
+        document.getElementById("commit").append(commitMsg);
+    });
+}
+lastCommit(username);
+
